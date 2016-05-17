13c0dc6b84796ef66b17941b8bc0ee6eb1791179
Alexander Tomokhov <alexoundos@gmail.com>
2016-05-18 01:44:27 +0300

    refactor lua script, add functions -> code reuse, error handling

--- cut.lua
+++ cut.lua
@@ -16,6 +16,8 @@ function init()
             -- create new file
             f = io.open(mp.get_property("path") .. ".sh", "w+")
         end
+
+        mp.register_event("shutdown", shutdown)
     end
 
     if hslibinit == nil then
@@ -26,58 +28,56 @@ function init()
     end
 end
 
-mp.register_event("shutdown", shutdown)
+function add(side)
+    init()
+    local timepos = mp.get_property("time-pos")
+    local retCode = hsAdd( f, mp.get_property("filename")
+                         , string.byte(side, 1), timepos )
+    if retCode == 0 then
+        mp.osd_message("add " .. side .. ":" .. timepos)
+    elseif retCode == 3 then
+        mp.osd_message(side .. ":" .. timepos .. " already added")
+    elseif retCode == 1 then
+        mp.osd_message("cannot parse existing script file")
+    else
+        mp.osd_message( "unknown error adding " .. side .. ":" .. timepos ..
+                        " for " .. mp.get_property("filename") )
+    end
+end
+
+function nav(direction)
+    init()
+    retCode, s, t = hsNav( f, mp.get_property("time-pos")
+                            , string.byte(direction, 1) )
+    print(retCode)
+    print(s)
+    print(t)
+    if retCode == 0 then
+        if s ~= nil then
+            mp.osd_message(string.char(s) .. ": " .. t)
+            mp.set_property("time-pos", t)
+        else
+            mp.osd_message("no " .. direction .. " timestamp")
+        end
+    elseif retCode == 1 then
+        mp.osd_message("cannot parse existing script file")
+    else
+        mp.osd_message( "unknown error while navigating " .. direction ..
+                        " in file " .. mp.get_property("filename") )
+    end
+end
 
 -- add A--> timestamp
-mp.add_forced_key_binding("a", function()
-    init()
-    mp.osd_message("A")
-    hsAdd( f, mp.get_property("filename")
-         , string.byte("A", 1), mp.get_property("time-pos") )
-end)
+mp.add_forced_key_binding("a", function() add("A") end)
 
 -- add -->B timestamp
-mp.add_forced_key_binding("b", function()
-    init()
-    mp.osd_message("B")
-    hsAdd( f, mp.get_property("filename")
-         , string.byte("B", 1), mp.get_property("time-pos") )
-end)
+mp.add_forced_key_binding("b", function() add("B") end)
 
--- navigate to previous timestamp
-mp.add_forced_key_binding("Ctrl+[", function()
-    init()
-    retCode, s, t = hsNav(f, mp.get_property("time-pos"), string.byte("b", 1))
-    print(retCode)
-    print(s)
-    print(t)
-    if retCode == 0 then
-        if s ~= 0 then
-            mp.osd_message(string.char(s) .. ": " .. t)
-            mp.set_property("time-pos", t)
-        else
-            mp.osd_message("no previous timestamp")
-        end
-    else
-        mp.osd_message("previous -> error " .. retCode)
-    end
-end)
+-- add -->X<-- timestamp
+mp.add_forced_key_binding("Ctrl+x", function() add("X") end)
 
 -- navigate to next timestamp
-mp.add_forced_key_binding("Ctrl+]", function()
-    init()
-    retCode, s, t = hsNav(f, mp.get_property("time-pos"), string.byte("f", 1))
-    print(retCode)
-    print(s)
-    print(t)
-    if retCode == 0 then
-        if s ~= 0 then
-            mp.osd_message(string.char(s) .. ": " .. t)
-            mp.set_property("time-pos", t)
-        else
-            mp.osd_message("no next timestamp")
-        end
-    else
-        mp.osd_message("next -> error " .. retCode)
-    end
-end)
+mp.add_forced_key_binding("Ctrl+]", function() nav("forward") end)
+
+-- navigate to previous timestamp
+mp.add_forced_key_binding("Ctrl+[", function() nav("backward") end)
