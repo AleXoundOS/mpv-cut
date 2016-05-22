40cd7f5ed4519698ec90faafb7d364e5ccd07e5a
Alexander Tomokhov <alexoundos@gmail.com>
2016-05-22 12:59:04 +0300

    handle 'nav' Position in lua and mpv, change file creation logic

--- cut.lua
+++ cut.lua
@@ -8,13 +8,17 @@ function shutdown()
     end
 end
 
-function init()
+function init(mode)
     if f == nil then
         -- first try to open existing file
         f = io.open(mp.get_property("path") .. ".sh", "r+")
-        if f == nil then
+        if f == nil and mode ~= "ro" then
             -- create new file
             f = io.open(mp.get_property("path") .. ".sh", "w+")
+        end
+
+        if f == nil then
+            return -1
         end
 
         mp.register_event("shutdown", shutdown)
@@ -46,7 +50,10 @@ function add(side)
 end
 
 function nav(direction)
-    init()
+    if init("ro") == -1 then
+        mp.osd_message("no script exists for this file")
+        return
+    end
     local timepos = mp.get_property("time-pos")
     -- return code, position (f_irst, o_nly, l_ast, n_one of these), side, time
     local retCode, p, s, t = hsNav(f, timepos, string.byte(direction, 1))
@@ -56,11 +63,11 @@ function nav(direction)
     print(t)
     if retCode == 0 then
         if p ~= nil then
-            if p == 'o' then
+            if p == string.byte('o') then
                 strPos = " (only)"
-            elseif p == 'f' then
+            elseif p == string.byte('f') then
                 strPos = " (first)"
-            elseif p == 'l' then
+            elseif p == string.byte('l') then
                 -- the supplied time is the only timestamp in script
                 strPos = " (last)"
             else
